9ea30fa63616bd4c98637a57541861ce32651a34
Christopher Klein <chrisklein303@gmail.com>
2011-08-24 10:03:19 -0600

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,22 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    
+    respond_to do |format|
+      if @user.save
+        # sign_in @user
+        format.html { redirect_to @user, :flash => { :success => "Welcome to the Sample App." }  }
+      else  
+        @title = "Sign up"
+        format.html { render 'new' }
+      end
+    end    
   end
 
 end
