29b8f1ff4a68bb44767298f64c1725b8047a98e9
Christopher Klein <chrisklein303@gmail.com>
2011-08-24 19:27:13 -0600

    Add session features for user signin/signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blocs::Application.routes.draw do
   
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy] 
 
   root :to => "pages#home"
   
@@ -8,8 +9,8 @@ Blocs::Application.routes.draw do
   match '/about',     :to => 'pages#about'
   match '/help',      :to => 'pages#help'
   match '/signup',    :to => 'users#new'
-  # match '/signin',    :to => 'sessions#new'
-  #  match '/signout',   :to => 'sessions#destroy'
+  match '/signin',    :to => 'sessions#new'
+  match '/signout',   :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
